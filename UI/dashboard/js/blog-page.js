7e92e9ee82938a8368c8e29f77aeeb9494c81f4d
Norbert Ishimwe <ishimwe96@gmail.com>
2023-02-23 22:21:41 +0200

    messages handler

--- UI/dashboard/js/blog-page.js
+++ UI/dashboard/js/blog-page.js
@@ -117,7 +117,6 @@ async function getPost(id) {
     } catch (error) {
       console.error(error);
     }
-   
     }
 
   })
--- UI/dashboard/js/messages.js
+++ UI/dashboard/js/messages.js
@@ -1,3 +1,5 @@
+//import { getMessages } from "../../../server/src/controllers/messages"
+
 const LoggedUser = localStorage.getItem('LoggedUser')
 
 if(!LoggedUser){
@@ -10,36 +12,36 @@ if(!LoggedUser){
    //alert('kk')
    window.localStorage.removeItem('LoggedUser')
    window.localStorage.removeItem('auth-token')
-
    location.href= '../index.html'
  })
 
- window.onload = getMessage();
+ window.onload = getMessagez();
 
- async function getMessage(){
-   const token = localStorage.getItem('auth-token')
-
-   const response = await fetch(`https://my-portfolio-production-2587.up.railway.app/messages`, {
+async function getMessagez(){
+const token = localStorage.getItem('auth-token')
+const response = await fetch(`https://my-portfolio-production-2587.up.railway.app/messages`, {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
         'Authorization': `Bearer ${token}`
       },})
 const result = await response.json();
- const getMessages = result.Messages;
-// console.log(getMessages);
+const getMessages = result.Messages;
+console.log(getMessages);
+const messageContainer = document.querySelector("#all-messages");
+
 if(!getMessages||getMessages.length == 0){
    //alert('null')
-   commentsContainer.innerHTML += `
+   messageContainer.innerHTML += `
    <h4 class='no-messages'> No messages received</h4>
-   `
+ `
 } 
 const table = () =>{
 
    getMessages.sort((a, b) => a.date - b.date).reverse();
    getMessages.forEach((element,index) => {
 //console.log(element.names);
-commentsContainer.innerHTML += `
+messageContainer.innerHTML += `
 <div class="comment-content message-content">
 <div class="user-delete">
    <h4>${element.name}</h4>
@@ -54,21 +56,47 @@ commentsContainer.innerHTML += `
 `
 });
 }
-
-
 table();
 
 }
- 
-const deleteMessage = (btn) => {
-      //console.log(btn);
-    if (btn > -1) { // only splice array when item is found
-        getMessages.splice(btn, 1); // 2nd parameter means remove one item only
-        window.localStorage.setItem("messages", JSON.stringify(getMessages));
-        alert("Message Deleted")
-        location.reload()
- }
+async function deleteMessage(btn){
+   const token = localStorage.getItem('auth-token')
+   const response = await fetch(`https://my-portfolio-production-2587.up.railway.app/messages`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': `Bearer ${token}`
+      },})
+const result = await response.json();
+let getMessages = result.Messages;
+
+getMessages.reverse()
+// reversedMessages.splice(btn, 1)
+// getMessages = reversedMessages.reverse()
+messageToDeleteId = getMessages[btn]._id
+const deleteMessage = await fetch(`https://my-portfolio-production-2587.up.railway.app/messages/delete/${messageToDeleteId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': `Bearer ${token}`
+      },})      
+const deleteMessageResponse = await deleteMessage.json();
+if(deleteMessageResponse.ok){
+   alert('deleted')
+
+location.reload()
+}
+else{
+   alert('error deleting')
+}
+   //console.log(btn);
+//     if (btn > -1) { // only splice array when item is found
+
+//         getMessages.splice(btn, 1); // 2nd parameter means remove one item only
+//         window.localStorage.setItem("messages", JSON.stringify(getMessages));
+//  }
  };
+
 
 const deleteAll = document.querySelector('.btn-danger')
 deleteAll.addEventListener('click',(e)=>{
